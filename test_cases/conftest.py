468e1dbf125528672d74c3ebb9e9c42721ed341a
sanath.singavarapu <sanath.singavarapu@infoworks.io>
2023-06-26 19:44:33 +0530

    Additional Variables for Replicator Test Cases.

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -5,7 +5,15 @@ class ValueStorage:
     source_id = "555739cc2a12b4664d09b44f"
     table_ids = ["629780ec1086072f3633ba4a"]
     table_group_id = "6297810e7c6c8f543ab516e4"
-    domain_id = "950f61da93956431ca173eb3"
+    # domain_id = "950f61da93956431ca173eb3"
     pipeline_id = "627b674c80cccd05ac2007c5"
     pipeline_version_id = "627b6bb680cccd05ac2007e7"
     workflow_id = "8bd61d6b91fd01249a02a0f5"
+
+    # Replication Configs
+    replicator_source_id = ""
+    replicator_destination_id = ""
+    replicator_domain_id = "6461e436a29d5d209112153b"
+    replicator_definition_id = ""
+    replicator_table_ids = []
+    replicator_definition_schedule_id = ""
