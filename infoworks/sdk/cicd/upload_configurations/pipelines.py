40f81f161ede1a02c5502e64635a6890d6b8501f
Abhishek R <abhr1994@gmail.com>
2023-08-10 10:48:00 +0530

    Removed custom tag support

--- infoworks/sdk/cicd/upload_configurations/pipelines.py
+++ infoworks/sdk/cicd/upload_configurations/pipelines.py
@@ -203,10 +203,8 @@ class Pipeline:
         # Add fields like query_tag, custom_tags
         if "query_tag" in self.configuration_obj["configuration"].get("pipeline_configs", {}):
             pipeline_json_object["query_tag"] = self.configuration_obj["configuration"].get("pipeline_configs", {}).get("query_tag")
-        if "custom_tags" in self.configuration_obj["configuration"].get("pipeline_configs", {}):
-            pipeline_json_object["custom_tags"] = self.configuration_obj["configuration"].get("pipeline_configs", {}).get("custom_tags")
-        # if "custom_tags" in self.configuration_obj["configuration"].get("pipeline_configs", {}):
-        #     pipeline_json_object["custom_tags"] = self.configuration_obj["configuration"].get("pipeline_configs", {}).get("custom_tags")
+        if "description" in self.configuration_obj["configuration"].get("pipeline_configs", {}):
+            pipeline_json_object["description"] = self.configuration_obj["configuration"].get("pipeline_configs", {}).get("description")
         # 5.3.x onwards CDW support
         if self.interactive_id is None:
             pipeline_json_object["run_job_on_data_plane"] = False
